7737293ef342c3aedb1af89af920450a9df84577
fatur fachsandy <faturfachsandy31@gmail.com>
2018-02-19 11:17:32 +0800

    Belajar Model dikit

--- app/Models/Nilai.php
+++ app/Models/Nilai.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use App\Models\SubnilaiNilai;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Nilai extends Model
 {
-    //
+    protected $fillable = ['nama_nilai', 'keterangan'];
+
+    public function subnilaiNilai 
+    {
+    	return $this->hasOne(SubnilaiNilai::class);
+    }
 }
--- app/Models/SubnilaiNilai.php
+++ app/Models/SubnilaiNilai.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use App\Models\Nilai;
 use Illuminate\Database\Eloquent\Model;
 
 class SubnilaiNilai extends Model
 {
-    //
+    protected $fillable = 
+    ['nama_subnilai', 'keterangan', 'default', 'urutan', 'berlaku_sejak', 'berlaku_hingga', 'aktif'];
+
+    public function nilai 
+    {
+    	return $this->belongsTo(Nilai::class);
+    }
 }
